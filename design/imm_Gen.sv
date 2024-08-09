1bcbd5532d13a5c1cb459f2fe7c93869dd389a28
Paulo Vitor <88457797+PauloV1@users.noreply.github.com>
2024-08-08 23:19:40 -0300

    Formato do Jal ajustado e adicionado o formato do Jalr

--- design/imm_Gen.sv
+++ design/imm_Gen.sv
@@ -27,8 +27,11 @@ module imm_Gen (
         1'b0
       };
 
+      7'b1100111: /*Jalr*/
+      Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
+
       7'b1101111: /*J-type*/
-      Imm_out = {inst_code[31] ? 11'h7FF : 11'b0, inst_code[19:12], inst_code[20], inst_code[30:25], inst_code[24:21], 1'b0};
+      Imm_out = {inst_code[31] ? 11'h7FF : 11'b0, inst_code[31], inst_code[19:12], inst_code[20], inst_code[30:21], 1'b0};
 
       7'b0110111: /*U-type*/
       Imm_out = {inst_code[31:12], 12'b0};
